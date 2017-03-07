978a2755bdc5e447d25a1931c431915151c2b61e
2005920 <2005920@AL-105-09.student.acalanes.k12.ca.us>
2017-03-07 11:43:30 -0800

    Trial

--- src/Activity01/Card.java
+++ src/Activity01/Card.java
@@ -1,3 +1,5 @@
+package Activity01;
+
 /**
  * Card.java
  *
@@ -33,6 +35,7 @@ public class Card {
 	 */
 	public Card(String cardRank, String cardSuit, int cardPointValue) {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+                //jkhjkhjkh
 	}
 
 
